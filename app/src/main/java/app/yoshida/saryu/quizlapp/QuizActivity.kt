b15caf552d3586de99ea047c16ba056098cbee38
KENKUN-1031 <kencoding1031@gmail.com>
2024-09-24 13:04:23 +0900

    refactor: :art: apiから取得した問題の表示

--- app/src/main/java/app/yoshida/saryu/quizlapp/QuizActivity.kt
+++ app/src/main/java/app/yoshida/saryu/quizlapp/QuizActivity.kt
@@ -7,22 +7,37 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import app.yoshida.saryu.quizlapp.databinding.ActivityQuizBinding
+import java.util.ArrayList
 
 class QuizActivity : AppCompatActivity() {
     private lateinit var binding: ActivityQuizBinding
 
+    var quizCount: Int = 0
+    var correctCount: Int = 0
+    var correctAnswer: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizBinding.inflate(layoutInflater).apply { setContentView(this.root)}
-        val quizList = intent.getSerializableExtra("GPT_RESPONSE") as? MutableList<*>
-
+        val quizList = intent.getSerializableExtra("GPT_RESPONSE") as? MutableList<ArrayList<String>>
+        // quizListにデータが入ってる
         if (quizList != null) {
-            // Iterate through the quizList and use it as needed
+            //この中に続きの処理を書いてもいい
+            displayQuestion(quizList)
             for (quiz in quizList) {
                 Log.d("Quiz", quiz.toString())
             }
         } else {
             Log.d("Quiz", "No data received!")
         }
+    }
+
+    fun displayQuestion(quizList: MutableList<ArrayList<String>>){
+        val question: List<String> = quizList[quizCount]
+        binding.quizText.text = question[0]
+        binding.answerButton1.text = question[1]
+        binding.answerButton2.text = question[2]
+        binding.answerButton3.text = question[3]
+
     }
 }
